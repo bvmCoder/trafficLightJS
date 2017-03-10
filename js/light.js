3cdb4098995b2f1e9391dee8d8762edfdbe60d20
Dixit Patel <talkwithdixit@gmail.com>
2017-03-10 08:01:03 -0500

    :fist: Removed Semi Colons

--- js/light.js
+++ js/light.js
@@ -1,46 +1,47 @@
 (function() {
-    'use strict';
+    'use strict'
 
-    function _id(id) {
-        return document.getElementById(id);
+    const _id = (id) => {
+        return document.getElementById(id)
     }
 
-    function _class(className) {
-        return document.getElementsByClassName(className);
+    const _class = (className) => {
+        return document.getElementsByClassName(className)
     }
 
     function _queSelAll(query) {
-        return document.querySelectorAll(query);
+        return document.querySelectorAll(query)
     }
 
-    var buttons = _queSelAll('.button');
-    var bulbs = _queSelAll('.bulb');
+    var buttons = _queSelAll('.button')
+    var bulbs = Array.from(_queSelAll('.bulb'))
 
     function clearLights() {
+
         Array.prototype.forEach.call(bulbs, function(bulb) {
-            //console.log(bulb.tagName);
-            bulb.style.backgroundColor = '#111111';
+            //console.log(bulb.tagName)
+            bulb.style.backgroundColor = '#111111'
         });
     }
 
     function turnOnLight(event) {
-        clearLights();
-        console.log(event);
-        var clickedElement = this;
+        clearLights()
+        console.log(event)
+        var clickedElement = this
 
         if (clickedElement.id === 'stopButton') {
-            _id('stopLight').style.backgroundColor = 'red';
+            _id('stopLight').style.backgroundColor = 'red'
         } else if (clickedElement.id === 'slowButton') {
-            _id('slowLight').style.backgroundColor = 'yellow';
+            _id('slowLight').style.backgroundColor = 'yellow'
         } else {
-            _id('goLight').style.backgroundColor = 'green';
+            _id('goLight').style.backgroundColor = 'green'
         }
     }
 
 
     Array.prototype.forEach.call(buttons, function(button) {
-        //console.log(button.tagName);
-        button["addEventListener"]('click', turnOnLight, false);
+        //console.log(button.tagName)
+        button["addEventListener"]('click', turnOnLight, false)
     });
 
 })();
